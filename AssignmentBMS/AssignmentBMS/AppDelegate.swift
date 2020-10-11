56cefd03557a6c74abadcba7d9b88cf4f46a80a1
Ajay Bhanushali <ajayrbhanushali@gmail.com>
2020-10-12 01:49:11 +0530

    Done: Root VC Set

--- AssignmentBMS/AssignmentBMS/AppDelegate.swift
+++ AssignmentBMS/AssignmentBMS/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  AssignmentBMS
 //
-//  Created by D2k on 09/10/20.
+//  Created by Ajay on 09/10/20.
 //  Copyright © 2020 Ajay Bhanushali. All rights reserved.
 //
 
@@ -11,27 +11,23 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setRoot()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    private func setRoot() {
+//        window = UIWindow()
+//        window?.rootViewController = MovieListVC()
+//        window?.makeKeyAndVisible()
+        window = UIWindow()
+        let moviesVC = MoviesModuleBuilder().buildModule()
+        let navigationController = UINavigationController(rootViewController: moviesVC)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
