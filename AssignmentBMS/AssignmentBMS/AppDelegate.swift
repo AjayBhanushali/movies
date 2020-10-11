4c746ffba7de0b20a395500d2d68677700b97049
Ajay Bhanushali <ajayrbhanushali@gmail.com>
2020-10-12 01:49:40 +0530

    Done: Appdelegate Cleaned

--- AssignmentBMS/AssignmentBMS/AppDelegate.swift
+++ AssignmentBMS/AssignmentBMS/AppDelegate.swift
@@ -14,15 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         setRoot()
         return true
     }
     
     private func setRoot() {
-//        window = UIWindow()
-//        window?.rootViewController = MovieListVC()
-//        window?.makeKeyAndVisible()
         window = UIWindow()
         let moviesVC = MoviesModuleBuilder().buildModule()
         let navigationController = UINavigationController(rootViewController: moviesVC)
